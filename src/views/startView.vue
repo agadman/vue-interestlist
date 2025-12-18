0b409ae559d11e7dae20af2c68297367cb229813
Annika Gadman <annika.gadman@gmail.com>
2025-12-18 08:36:14 -0600

    init vue project

---
+++ src/views/startView.vue
@@ -0,0 +1,12 @@
+<template>
+    <h2>Startsida</h2>
+    <p>Här ska jag ha en banner bild samt kort informaion om mitt fritidsintresse </p>
+</template> 
+
+<script setup>
+
+</script>
+
+<style scoped>
+    
+</style>
